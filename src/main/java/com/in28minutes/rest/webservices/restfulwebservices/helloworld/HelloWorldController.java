9dbfbf9c538b4de4bd0779e1de7d67b242756583
dpal6025 <dpal6025@gmail.com>
2020-07-28 12:40:21 +0530

    second commit

--- src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
+++ src/main/java/com/in28minutes/rest/webservices/restfulwebservices/helloworld/HelloWorldController.java
@@ -15,7 +15,7 @@ public class HelloWorldController {
 
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
-		return "Hello World";
+		return "Hello Welcome World";
 	}
 
 	@GetMapping(path = "/hello-world-bean")
